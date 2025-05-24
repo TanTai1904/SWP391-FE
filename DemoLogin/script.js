1736d0ee3683a6454276a5ad1d04fbd1104d6d10
TanTai1904 <taitqtse182652@fpt.edu.vn>
2025-05-25 01:42:17 +0700

    demo login

---
+++ DemoLogin/script.js
@@ -0,0 +1,4 @@
+document.getElementById("registerLink").addEventListener("click", function (e) {
+  e.preventDefault(); // ngăn chuyển hướng mặc định (nếu cần)
+  window.location.href = "register.html"; // chuyển hướng đến trang đăng ký
+});
