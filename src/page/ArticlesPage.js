2fb7e7f70062d3372369868dad8ab7c21702902a
TanTai1904 <taitqtse182652@fpt.edu.vn>
2025-05-28 10:44:00 +0700

    giaodienhomage

---
+++ src/page/ArticlesPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+const ArticlesPage = () => {
+    return (
+        <div>
+            <h1>Articles Page</h1>
+        </div>
+    );
+};
+
+export default ArticlesPage;
