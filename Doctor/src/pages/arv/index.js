1606a83afed8bccf0195da77770b2742c8248461
TanTai1904 <taitqtse182652@fpt.edu.vn>
2025-07-09 21:32:48 +0700

    doctor

--- Doctor/src/pages/arv/index.js
+++
@@ -1,4 +0,0 @@
-import ARVRegimenDetail from '../arv/ARVRegimenDetail';
-
-export { default as ARVComponentList } from './ARVComponentList';
-export { default as ARVRegimenList } from './ARVRegimenList';
